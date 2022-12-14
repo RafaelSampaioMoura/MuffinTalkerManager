5153a2b225d414e12c0edc19c5d21dc4fe730df6
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2022-12-14 20:29:50 -0300

    Requisito 01 completo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const path = '/talker.json';
 
 const app = express();
 app.use(express.json());
@@ -7,8 +11,18 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/', async (_request, _response) => {
+  console.log('Servidor ouvindo porta 3000');
+});
+
+app.get('/talker', async (_req, res) => {
+  const completePath = join(__dirname, path);
+  const data = await fs.readFile(completePath);
+  if (data.length === 0) {
+    return res.status(HTTP_OK_STATUS).send([]);
+  } 
+    const allTalkers = JSON.parse(data);
+    res.status(HTTP_OK_STATUS).send(allTalkers);
 });
 
 app.listen(PORT, () => {
