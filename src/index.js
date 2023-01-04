058430490d669177807f34abee4990d96f136f77
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-01-04 17:56:31 -0300

    Test Suites 3 e 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs").promises;
 const { join } = require("path");
+const tokenGenerator = require("crypto");
 
 const path = "/talker.json";
 
@@ -8,8 +9,43 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_BAD_REQUEST_STATUS = 400;
 const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = "3000";
+
+const verificarEmail = (req, res, next) => {
+  const { email } = req.body;
+  const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  if (!email) {
+    res.status(HTTP_BAD_REQUEST_STATUS).send({
+      message: 'O campo "email" é obrigatório',
+    });
+  } else if (!email.match(regex)) {
+    res.status(HTTP_BAD_REQUEST_STATUS).send({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  } else {
+    next();
+  }
+};
+
+const verificarSenha = (req, res, next) => {
+  const { password } = req.body;
+  const regex = 6;
+
+  if (!password) {
+    res.status(HTTP_BAD_REQUEST_STATUS).send({
+      message: 'O campo "password" é obrigatório',
+    });
+  } else if (password.length < regex) {
+    res.status(HTTP_BAD_REQUEST_STATUS).send({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  } else {
+    next();
+  }
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get("/talker/:id", async (req, res) => {
@@ -37,6 +73,13 @@ app.get("/talker", async (_req, res) => {
   }
   const allTalkers = JSON.parse(data);
   res.status(HTTP_OK_STATUS).send(allTalkers);
+});
+
+app.post("/login", verificarEmail, verificarSenha, async (__req, res) => {
+  const rndToken = tokenGenerator.randomBytes(8).toString("hex");
+  res.status(HTTP_OK_STATUS).send({
+    token: rndToken,
+  });
 });
 
 app.listen(PORT, () => {
